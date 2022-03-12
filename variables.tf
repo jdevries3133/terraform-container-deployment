c2fa8036557644cf30c09a8c7f43c6e0a42f6152
Jack DeVries <jdevries3133@gmail.com>
2022-03-12 17:23:58 -0500

    fix descriptions

--- variables.tf
+++ variables.tf
@@ -5,28 +5,28 @@ variable "app_name" {
 
 variable "container" {
   type = string
-  name = "fully qualified identifer of the container, like `jdevries3133/container:latest`"
+  description = "fully qualified identifer of the container, like `jdevries3133/container:latest`"
 }
 
 variable "domain" {
   type = string
-  name = "domain name of the app, which will be put into an ingress_v1 rule"
+  description = "domain name of the app, which will be put into an ingress_v1 rule"
 }
 
 variable "extra_env" {
   type = map(string)
   default = {}
-  name = "extra environment variables to pass to the container"
+  description = "extra environment variables to pass to the container"
 }
 
 variable "replica_count" {
   type = number
   default = 1
-  name = "number of replicas in the deployment"
+  description = "number of replicas in the deployment"
 }
 
 variable "application_port" {
   type = number
   default = 80
-  name = "port that the application exposes, which you would like to expose to the outside world"
+  description = "port that the application exposes, which you would like to expose to the outside world"
 }
